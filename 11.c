d13730e4c17181236e68d1748829e21250a6f3e3
alex-kalinin-jr <kalinakrasnaya1990@gmail.com>
2022-04-18 15:11:57 +0700

    parsing

--- 11.c
+++ 11.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 #include "my.h"
 
+
 int main() {
     char expression[50];
     struct queue my = {NULL, NULL};
-
-
-    if (scanf("%s", expression) != 1) {
+    if (scanf("%[^\n]s", expression) != 1) {
         printf("N/A");
     } else {
         parse(expression, &my);
-
-
+        printf("%d\n", my.head -> node.code);
+        printf("%d\n", my.head -> next -> node.code);
+        printf("%d\n", my.head -> next -> next -> node.code);
+        printf("%d\n", my.tail -> node.code);
     }
 
 
@@ -22,12 +23,11 @@ int main() {
 void parse(char *string, struct queue *my) {
     char *pr = string;
     while (*pr) {
-        if (*pr == '+') {
+        if (*pr == 'x')
+            enqueue(my, VAR, 0, 0);
+        if (*pr == '+')
             enqueue(my, PLUS, 0, LOW);
-        }
-        if ((pr[0] > 47 && pr[1] < 58) || (pr[0] = '-' && *(pr - 1) == '('))
-            enqueue(my, NUM, strtod(pr, &pr), 0);
-        if (*pr == '-' && !(*(pr + 1) > 48 && *(pr + 1) < 58))
+        if (*pr == '-')
             enqueue(my, MINUS, 0, LOW);
         if (*pr == '*')
             enqueue(my, MULT, 0, MEDIUM);
@@ -37,35 +37,38 @@ void parse(char *string, struct queue *my) {
             enqueue(my, LEFTBR, 0, 0);
         if (*pr == ')')
             enqueue(my, RIGHTBR, 0, 0);
-        if (pr[0] == 'S' && pr[1] == 'I' && pr[2] == 'N')
+        if (pr[0] == 's') {
             enqueue(my, SIN, 0, HIGHT);
-        if (pr[0] == 'C' && pr[1] == 'O' && pr[2] == 'S')
+        }
+        if (pr[0] == 'c' && pr[1] == 'o' && pr[2] == 's') {
             enqueue(my, COS, 0, HIGHT);
-        if (pr[0] == 'T' && pr[1] == 'A' && pr[2] == 'N')
+            pr += 2;
+        }
+        if (pr[0] == 't' && pr[1] == 'a' && pr[2] == 'n') {
             enqueue(my, TAN, 0, HIGHT);
-        if (pr[0] == 'C' && pr[1] == 'T' && pr[2] == 'G')
+            pr += 2;
+        }
+        if (pr[0] == 'c' && pr[1] == 't' && pr[2] == 'g') {
             enqueue(my, CTG, 0, HIGHT);
-        if (pr[0] == 'S' && pr[1] == 'Q' && pr[2] == 'R' && pr[3] == 'T')
+            pr += 2;
+        }
+        if (pr[0] == 's' && pr[1] == 'q' && pr[2] == 'r' && pr[3] == 't') {
             enqueue(my, SQRT, 0, HIGHT);
-        if (pr[0] == 'L' && pr[1] == 'N')
+            pr += 3;
+        }
+        if (pr[0] == 'l' && pr[1] == 'n') {
             enqueue(my, LN, 0, HIGHT);
-        if (pr[0] == 'x')
-            enqueue(my, VAR, 0, 0);
-        pr++;
+            pr += 1;
+        }
+
+        if ((*pr > 47 && *pr < 58) || (*pr = '-' && *(pr - 1) == '(')) {
+            enqueue(my, NUM, strtod(pr, &pr), 0);
+            pr--;
+        }
+
+        pr += 1;
     }
 }
 
 
-    // struct queue my = {NULL, NULL};
-    // enqueue(&my, 0, 23, 1);
-    // enqueue(&my, 0, 24, 1);
-    // enqueue(&my, 0, 25, 1);
-    // enqueue(&my, 0, 26, 1);
 
-    // printf("%f", my.head -> node.number);
-    // printf("%f", my.tail -> node.number);
-
-    // struct list *a = deque(&my);
-    // printf("\n%f\n", a -> node.number);
-    // printf("%f", my.head -> node.number);
-    // printf("%f", my.tail -> node.number);
